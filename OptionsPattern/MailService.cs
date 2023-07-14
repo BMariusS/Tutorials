0ac081cdd4141983f2a5c3ad136fe1a5f187bb7a
BMariusS <31067789+BMariusS@users.noreply.github.com>
2023-07-14 20:46:38 +0300

    Added commented code to demonstrate without options pattern

--- OptionsPattern/MailService.cs
+++ OptionsPattern/MailService.cs
@@ -5,15 +5,34 @@ namespace OptionsPattern
     public class MailService
     {
         private readonly MailSettingsOptions? _mailSettings;
+        
+        // Code used to demonstrate without options pattern
+        // private readonly IConfiguration _configuration;
+
+        // public MailService(IOptions<MailSettingsOptions> mailSettings, IConfiguration configuration)
+        // {
+        //     _configuration = configuration;
+        //     _mailSettings = mailSettings.Value;
+        // }
 
         public MailService(IOptions<MailSettingsOptions> mailSettings)
         {
             _mailSettings = mailSettings.Value;
         }
-
         public string? FormatMailToString()
         {
-            var mailString = $$"""
+            // Code used to demonstrate without options pattern
+//              var mailBaseUri = _configuration.GetValue<string>("MailSettings:BaseUri");
+//              var mailApiKey = _configuration.GetValue<string>("MailSettings:ApiKey");
+//              var mailDomain = _configuration.GetValue<string>("MailSettings:Domain");
+//              var mailFrom = _configuration.GetValue<string>("MailSettings:From");
+//              var mailString = $$"""
+//                                   BaseUri = {{mailBaseUri}},
+//                                   ApiKey = {{mailApiKey}},
+//                                   Domain = {{mailDomain}},
+//                                   From = {{mailFrom}}
+//                                  """;
+             var mailString = $$"""
                                      BaseUri = {{_mailSettings?.BaseUri}},
                                      ApiKey = {{_mailSettings?.ApiKey}}, 
                                      Domain = {{_mailSettings?.Domain}}, 
